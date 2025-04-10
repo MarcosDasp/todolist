2aa22dbab6cf994a5df5e15b42bc635825c77e8d
MarcosDasp <marcos_dasped@gmail.com>
2025-04-10 19:06:44 -0300

    tentando

--- src/main/java/com/mrcsdsp/todolist/task/TaskController.java
+++ src/main/java/com/mrcsdsp/todolist/task/TaskController.java
@@ -7,6 +7,7 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +20,7 @@ import com.mrcsdsp.todolist.utils.Utils;
 
 import jakarta.servlet.http.HttpServletRequest;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/tasks")
 public class TaskController {
